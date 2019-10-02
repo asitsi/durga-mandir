04b08c8f8343d86bdbc531416a4b59f122476e10
asitsi <Asitsingh0@gmail.com>
2019-10-03 00:01:31 +0530

    first.commit

---
+++ index.php
@@ -0,0 +1,437 @@
+<?php?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+
+    <nav class="navbar navbar-expand-lg " style="background-color: #F34B44;color: #E69A8DFF">
+    	<div class="container">
+    <a class="navbar-brand" href="#"><img src="img/Peace_Logo.png"></a>
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <i class="fa fa-bars" aria-hidden="true"></i>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto" >
+      <li class="nav-item active">
+        <a class="nav-link active" style="color:white" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" style="color:#616247FF" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+           About Sansthan 
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="pages/about.php">About</a>
+          <a class="dropdown-item" href="#">History</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" style="color:#616247FF" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Sansthan Literature 
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Sai Satcharitra</a>
+          <a class="dropdown-item" href="#">Aarti</a>
+          <a class="dropdown-item" href="#">Sai Leela</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Sai Arpan</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" style="color:#616247FF" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Shirdi Tourism 
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+        
+          <a class="dropdown-item" href="pages/howtoreach.php">How to reach shridi</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="pages/placearoundus.php">Place to visit</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle"style="color:#616247FF" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Online Services  
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" style="color:#616247FF" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+           Media   
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="pages/SatcharitraAudio.php">Audio</a>
+          <a class="dropdown-item" href="pages/satcharitraVideo.php">Video</a>
+          <a class="dropdown-item" href="pages/matagallery.php">Downloads</a>
+          <a class="dropdown-item" href="#">Centenary Events</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="pages\Invitation.php">Invitations</a>
+        </div>
+      </li>
+       <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" style="color:#616247FF" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+           Contact   
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="pages/contact.php">Department</a>
+          <a class="dropdown-item" href="pages/FAQ.php">FAQ</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="pages/Feedback.php">Feedback</a>
+        </div>
+      </li>
+    </ul>
+    
+    </div>
+   </div> 
+ </nav>
+
+<!--slideshow-->
+
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img src="http://localhost/Temple/img/durga/IMG_3592.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="http://localhost/Temple/img/durga/IMG_3594.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="http://localhost/Temple/img/durga/IMG_3595.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+
+
+</div>
+
+<!-- about-->
+
+<div class="aboutus" style="text-align: center">
+    <audio src="audio/hariomnaath.mp3"  controls="" volume="" style="width: 100%;"></audio><br><br>
+	<h4><u><b>About us</b></u></h4><br>
+	<p>The small temple of Shri Durga ji was made big in 1966–67<br> and the huge statue of Shri Durga ji was installed in a grand and sober atmosphere with Vedic method.<br> Just below this temple, a cave was built for meditation. A huge yagyashala was constructed to the south of this temple.<br> The miniature statue of Shri Durga was installed in a small mathiya in the foot of Yagyashala...</p><br>
+    <a href="pages/about.php">
+      <button type="button" class="btn btn-outline-dark rounded-pill" style="padding-left: 2%;padding-right: 2%">More</button>
+   </a>
+</div>
+
+<!--home about us-->
+<div class="dailyrotine">
+<dir style="padding-top: 40px " class="">
+<div class="container">
+  <dir class="row">
+    <div class="col-lg-3 col-md-3 col-12">
+      <div class="">
+        <div class="card" style="width: 100%; background-color: #e2534c;color: #fff;padding: 20px;border:none;">
+  <div class="card-body">
+    <h5 class="card-title">News</h5>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="padding: 15px">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <p>विश्व हिंदू परिषद ने मोदी सरकार को दी 'डेडलाइन', कहा- राम मंदिर के लिए संसद में लाएं अध्यादेश</p>
+    </div>
+    <div class="carousel-item">
+      <p>विश्व हिंदू परिषद ने मोदी सरकार को दी 'डेडलाइन', कहा- राम मंदिर के लिए संसद में लाएं अध्यादेश</p>
+      
+    </div>
+    <div class="carousel-item">
+      <p>विश्व हिंदू परिषद ने मोदी सरकार को दी 'डेडलाइन', कहा- राम मंदिर के लिए संसद में लाएं अध्यादेश</p>
+      
+    </div>
+  </div>
+</div>
+      <button type="button" class="btn btn-outline-light" style="border-radius: 25px; ">Read more</button>
+  </div>
+</div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-12">
+      <div class="">
+     <div class="card" style="width: 100%;background-color: #FCE77D;color: #F96167;padding: 20px">
+  <div class="card-body">
+    <img src="http://localhost/Temple/img/live1.png" style="width: 6rem;margin: auto;display: block">
+    <h6 class="card-subtitle mb-2 text-muted"></h6><br>
+    <h4 class="card-text" style="text-align: center">Live Darshan</h4><br>
+      <button type="button" class="btn btn-outline-danger" style="border-radius: 25px; margin: auto;display: block; ">More</button></br>
+    
+  </div>
+</div>
+
+   </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-12">
+      <div class="">
+     <div class="card" style="width: 100%;background-color:#FC766AFF;color: #5B84B1FF  ;padding: 20px">
+  <div class="card-body">
+    <img src="http://localhost/Temple/img/donate.png" style="width: 6rem;margin: auto;display: block">
+    <h6 class="card-subtitle mb-2 text-muted"></h6><br>
+    <h4 class="card-text" style="text-align: center">Donation NOW</h4><br>
+      <button type="button" class="btn btn-outline-danger" style="border-radius: 25px; margin: auto;display: block; ">More</button></br>
+    
+  </div>
+</div>
+
+   </div>
+      
+    </div>
+    <div class="col-lg-3 col-md-3 col-12">
+      
+   <div class="">
+     <div class="card" style="width: 100%;background-color: #FAD0C9FF;color: #6E6E6DFF;padding: 20px">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <img src="http://localhost/Temple/img/location.png" style="width: 5rem;margin: auto;display: block">
+    <h6 class="card-subtitle mb-2 text-muted"></h6><br>
+    <h4 class="card-text" style="text-align: center">Location</h4><br>
+    <a href="pages/location.php" style="text-decoration:none; ">
+        <button type="button" class="btn btn-outline-danger animated  bounceInUp " style="border-radius: 25px; margin: auto;display: block; ">More</button>
+     </a>
+     <br>
+   </div>
+  </div>
+ </div>
+</div>
+  </dir>
+</div><br><br>
+</div>
+
+ 
+  
+  <div class="" style="background-color: #e2534c"><br>
+ <div class="container">
+   <div class="row">
+     <div class="col-lg-6 col-md-6 col-12" style="background-color:#7fe5f0 ;padding: 20px;color: #666666 ">
+      
+     <table class="table table-hover" style="color: #808080;" >
+
+<h4 class="" style="text-align: center;"><i class="fa  fa-clock-o" aria-hidden="true" style=""></i>    Daily Programme</h4>
+<br>
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Time</th>
+      <th scope="col">Programme</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td><b>4:00am</b></td>
+      <td>Temple Open</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td><b>4:30am</b></td>
+      <td>Aarti</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td >Larry the Bird</td>
+      <td>Thornton</td>
+    </tr>
+  </tbody>
+</table>
+ 
+       <button type="button" class="btn btn-outline-danger" style="border-radius: 25px; margin: auto;display: block; ">Read More</button>
+      
+
+
+     </div>
+     <div class="col-lg-6 col-md-6 col-12" style="background-color: #ccff00;padding: 20px;text-align: center;">
+        <h4 class="" style="text-align: center;color: #666666"><i class="fa fa-calendar-plus-o" aria-hidden="true" style="margin-left: 12px;"></i>    Upcoming Festivals</h4><br>
+        <img src="http://localhost/Temple/img/gallery2.jpg" style="width: 7rem;margin: auto;display: block"><br>
+        <h6 style="color: #808080;" >29-September-2019</h6>
+        <h5 style="color: #808080;" ><b>NAVARATRI BEGINS</b></h5><br>
+       <button type="button" class="btn btn-outline-danger" style="border-radius: 25px; margin: auto;display: block; ">Read More</button>
+
+     </div>
+   </div>
+  </div><br>
+ </div>
+
+
+
+
+
+
+<!-- Fastivals-->
+<div style="background-color: #ffff66"><br><br>
+<div class="container" style="text-align: center; " >
+	<h2>Members details</h2><br><br>
+     <div class="row">
+     	<div class="col-lg-4 col-md-4 col-12">
+     		<div class="card" style="width: 18rem;">
+  <img src="img/pandit1.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Member name</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-outline-warning rounded-pill">contact</a>
+  </div>
+</div>
+     	</div>
+     	<div class="col-lg-4 col-md-4 col-12">
+     		<div class="card" style="width: 18rem;">
+  <img src="img/pandit2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Member name</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-outline-warning rounded-pill">contact</a>
+  </div>
+</div>
+     	</div>
+     	<div class="col-lg-4 col-md-4 col-12"><div class="card" style="width: 18rem;">
+  <img src="img/pandit3.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Member name</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-outline-warning rounded-pill" >contact</a>
+  </div>
+</div></div>
+     </div>
+</div>
+<br><br>
+</div>
+
+
+<!-- footer-->
+
+<div class="" style="background-color: #e2534c;"><br><br>
+      <div class="container" style="color: white">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-12">
+            <h4>Information</h4>
+            -------
+            <div class="row"><div class="col-lg-6 col-md-6 col-sm-6"><a href="" style="color: white"> Work Tenure</a></div>
+              
+            <div class="col-lg-6 col-md-6 col-sm-6"><a href="" style="color: white">Appeal</a></div>
+            </div><br>
+            <div class="row"><div class="col-lg-6 col-md-6 col-sm-6"><a href="" style="color: white">RTI Act Section-4</a>
+            </div>
+              
+            <div class="col-lg-6 col-md-6 col-sm-6"><a href="" style="color: white">Annual Reports</a></div>
+            </div><br>
+            <div class="row"><div class="col-lg-6 col-md-6 col-sm-6"><a href="" style="color: white">Sansthan Regulations</a></div>
+              
+            <div class="col-lg-6 col-md-6 col-sm-6"><a href="" style="color: white">Resolutions</a></div>
+            </div><br>
+            <div class="row"><div class="col-lg-6 col-md-6 col-sm-6"><a href="" style="color: white">Screen Reader Access</a></div>
+              
+            <div class="col-lg-6 col-md-6 col-sm-6"><a href="" style="color: white">Notice</a></div>
+            </div><br>
+            <div class="row"><div class="col-lg-6 col-md-6 col-sm-6"><a href="" style="color: white">Statistical Reports</a></div>
+              
+            <div class="col-lg-6 col-md-6 col-sm-6"><a href="" style="color: white"></a></div>
+            </div>
+            
+            <!--icons-->
+              
+             <div class="icons">
+              
+               <a href=""  target="_blank">
+                 <i class="fa fa-2x fa-facebook"></i>
+               </a>
+               <a href="" target="_blank">
+                 <i class="fa fa-2x fa-instagram"></i>
+               </a>
+               <a href="" target="_blank">
+                 <i class="fa fa-2x fa-twitter"></i>
+               </a>
+               <a href="" target="_blank">
+                 <i class="fa fa-2x fa-youtube"></i>
+               </a>
+             </div> 
+            
+            <p style="text-align: left">For Online Services please do contact on<br>
+02423-258956, 02423-258963,</p>
+          </div>
+          <div class="col-lg-6 col-md-6 col-12">
+            <h4>Feedback</h4>
+            -------
+<form action="pages/connect.php" method="post">
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="firstName">First Name</label>
+      <input type="name" class="form-control rounded-pill" id="firstName" name="firstName" placeholder="Firstname">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="lastName">Last Name</label>
+      <input type="lastName" class="form-control rounded-pill" id="lastName" name="lastName" placeholder="lastName">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="address">Address</label>
+    <input type="text" class="form-control rounded-pill" id="address" name="address" placeholder="1234 Main St">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Address 2</label>
+    <input type="text" class="form-control rounded-pill" id="inputAddress2" placeholder="Apartment, studio, or floor">
+  </div>
+  <div class="form-group">
+    <label for="message">Message</label>
+    <textarea class="form-control" id="message" name="message" rows="3"></textarea>
+  </div>
+  <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+      <button type="submit" class="btn btn-outline-light" style="border-radius: 25px; ">Send</button>
+</form>
+          </div>
+        </div>
+      </div> <br><br><hr>
+         
+         <div class="container" style="color: white;text-align: center;">
+           <div class="row">
+             <div class="col-lg-6 col-md-6 col-12">
+               <p>© Copyright 1998-2019 Durga mandir Sansthan Trust, Kanpur</p>
+             </div>
+
+             <div class="col-lg-6 col-md-6 col-12">
+              <a href="http://flyleaf.co.in" style="color: white;text-decoration: none;"> <p>@Flyleaf IT Ltd </p> </a>
+             </div>
+           </div>
+         </div>
+
+
+    </div>
+    
+  
+ <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <iframe src="https://www.youtube.com/watch?v=8dWjeeI5hmU" width="0" height="0" frameborder="0"></iframe>
+</html>
